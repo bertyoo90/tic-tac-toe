0b2b4a96cbdcf5612859fb16c9efac15bf3a2ad6
Albert Yoo <albertyoo@Alberts-MacBook-Pro.local>
2021-09-17 18:26:14 -0400

    Player(s) unable to make moves after there is winenr

--- script.js
+++ script.js
@@ -3,20 +3,21 @@
     
     //Tic-Tac-Toe board pieces and DOM elements
     let ticTacToeBoard = (function(){
-        let topLeft = document.querySelector('#top-left')
-        let topMid = document.querySelector('#top-mid')
-        let topRight = document.querySelector('#top-right')
-        let midLeft = document.querySelector('#mid-left')
-        let midMid = document.querySelector('#mid-mid')
-        let midRight = document.querySelector('#mid-right')
-        let botLeft = document.querySelector('#bot-left')
-        let botMid = document.querySelector('#bot-mid')
-        let botRight = document.querySelector('#bot-right')
-        let gameBoard = [topLeft, topMid, topRight,
-                        midLeft, midMid, midRight,
-                        botLeft, botMid, botRight]
 
-        let winningCombination = 
+            topLeft= document.querySelector('#top-left'),
+            topMid= document.querySelector('#top-mid'),
+            topRight= document.querySelector('#top-right'),
+            midLeft= document.querySelector('#mid-left'),
+            midMid= document.querySelector('#mid-mid'),
+            midRight= document.querySelector('#mid-right'),
+            botLeft= document.querySelector('#bot-left'),
+            botMid= document.querySelector('#bot-mid'),
+            botRight= document.querySelector('#bot-right'),
+            gameBoard= [topLeft, topMid, topRight,
+                        midLeft, midMid, midRight,
+                        botLeft, botMid, botRight],
+
+            winningCombination=
                         [[topLeft, topMid, topRight],
                         [midLeft, midMid, midRight],
                         [botLeft, botMid, botRight],
@@ -24,7 +25,9 @@
                         [topMid, midMid, botMid],
                         [topRight, midRight, botRight],
                         [topLeft, midMid, botRight],
-                        [topRight, midMid, botLeft]];
+                        [topRight, midMid, botLeft]]
+        
+        
             
 
         return {gameBoard, winningCombination} 
@@ -38,46 +41,53 @@
 
         //Logic that creates player moves for both players
         let playerMoves = (function(){
-            let isGameOver = false;
-            let turn = true;
-            let playerOne = Player('player-one', 1, 'X', false)
-            let playerTwo = Player('player-two', 2, 'O', false)
+            //Object that stores components of the game
+            const gameObj = {
+            isGameOver: false,
+            turn: true,
+            playerOne: Player('player-one', 1, 'X', false),
+            playerTwo: Player('player-two', 2, 'O', false)
+            }
+            
             let checkWin = () => {
-                for (let x of ticTacToeBoard.winningCombination) {
-                    if (x.every(i => i.textContent === playerOne.marker)) {
-                        console.log('player one wins!');
-                        isGameOver = true;
-                        playerOne.winStatus = true;
-                        console.log(isGameOver)
-                        return isGameOver
-                    } else if (x.every(i => i.textContent === playerTwo.marker)) {
-                        console.log('player two wins!');
-                        isGameOver = true;
-                        playerTwo.winStatus = true;
-                        console.log(isGameOver)
-                        return isGameOver
+                for (let x of ticTacToeBoard.board.winningCombination) {
+                    if (x.every(i => i.textContent === gameObj.playerOne.marker)) {
+                        alert('player one wins!');
+                        gameObj.isGameOver = true;
+                        gameObj.playerOne.winStatus = true;
+                        console.log(gameObj.isGameOver)
+                        return gameObj.isGameOver
+                    } else if (x.every(i => i.textContent === gameObj.playerTwo.marker)) {
+                        alert('player two wins!');
+                        gameObj.isGameOver = true;
+                        gameObj.playerTwo.winStatus = true;
+                        console.log(gameObj.isGameOver)
+                        return gameObj.isGameOver
                     }
-                    }
+                  }
                 }
-
+           
+            
             //Function that alternates players turns
             let markBoard = () => {
-                ticTacToeBoard.winningCombination;  
-                for (let place of ticTacToeBoard.gameBoard) {
+                ticTacToeBoard.board.winningCombination;  
+                for (let place of ticTacToeBoard.board.gameBoard) {
                     place.addEventListener('click', function(e){
                     //   let p = e.target.parentElement
                     //   let index = Array.prototype.indexOf.call(p.children, e.target)
-                      if (e.target.textContent == '' && !checkWin()){
-                        if (turn) {
-                          turn = false; 
-                          e.target.textContent = playerOne.marker
-                          return turn;
-                        } else if (!turn){
-                          turn = true;
-                          e.target.textContent = playerTwo.marker 
-                          return turn;
+                      if (e.target.textContent == '' && !gameObj.isGameOver){
+                        if (gameObj.turn) {
+                          gameObj.turn = false; 
+                          e.target.textContent = gameObj.playerOne.marker
+                          checkWin()
+                          return gameObj.turn;
+                        } else if (!gameObj.turn){
+                          gameObj.turn = true;
+                          e.target.textContent = gameObj.playerTwo.marker 
+                          checkWin()
+                          return gameObj.turn;
                         }
-                      }
+                      } 
                     })
                 }
                 
@@ -85,39 +95,36 @@
 
             markBoard()
 
-            return {playerOne, 
-                    playerTwo, 
-                    markBoard,
-                    turn,
-                    isGameOver,
-                    checkWin}
+            return {markBoard,
+                    checkWin,
+                    gameObj}
 
         
         })()
 
 
-        let game = (function(){
-            let endGame = false
-            let board = ticTacToeBoard.gameBoard
+        // let game = (function(){
+        //     let endGame = false
+        //     let board = ticTacToeBoard.gameBoard
 
-            function restartGame() {
-                for (squares of board){
-                    board.textContent = ''
-                }
-            }
+        //     function restartGame() {
+        //         for (squares of board){
+        //             board.textContent = ''
+        //         }
+        //     }
             
-            function winnerAnnouncement() {
-                playerMoves.playerOne.score = 0
-                playerMoves.playerTwo.score = 0
-                if (playerMoves.playerOne.winStatus = true){
-                    playerMoves.playerOne.score += 1
-                } if (playerMoves.playerTwo.winStatus = true){
-                    playerMoves.playerTwo.score += 1
-                }
-            }
+        //     function winnerAnnouncement() {
+        //     //     playerMoves.playerOne.score = 0
+        //     //     playerMoves.playerTwo.score = 0
+        //     //     if (playerMoves.playerOne.winStatus = true){
+        //     //         playerMoves.playerOne.score += 1
+        //     //     } if (playerMoves.playerTwo.winStatus = true){
+        //     //         playerMoves.playerTwo.score += 1
+        //     //     }
+        //     // }
 
-            winnerAnnouncement()
+        //     winnerAnnouncement()
 
-            return {winnerAnnouncement}
+        //     return {winnerAnnouncement}
             
-        })()
+        // })()
